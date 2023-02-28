ed3c616b619f906822a924bafa16f1d958b08ac0
ABiviano13 <abiviano0@outlook.com>
2023-02-28 15:51:18 +0100

    FEAT create component AppTitle + STAMP

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<!-- HTML -->
+<template>
+    <div>
+        <h1>
+            {{ title }}
+        </h1>
+    </div>
+</template>
+
+<!-- JS -->
+<script>
+
+export default {
+  data(){
+    return {
+        title: 'La mia prima app con Vite!'
+    }
+  }
+}
+
+</script>
+
+<!-- CSS (se serve) -->
