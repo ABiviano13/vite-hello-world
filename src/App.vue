e675cb3336b38935101292776a486bd06aef4450
ABiviano13 <abiviano0@outlook.com>
2023-02-28 16:35:43 +0100

    BONUS

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script>
 //1. si importa il file
-import AppTitle from './components/AppTitle.vue';
+import AppTitle from './components/AppTitle.vue'
+
+import AppHero from './components/AppHero.vue'
+
 
 export default {
   components: {
     //2. si registra il componente
-    AppTitle
+    AppTitle,
+    AppHero
+    
   },
 }
 </script>
 
 <template>
   <!-- 3. si utilizza il componente -->
+  <div>
+    <AppHero></AppHero>
+  </div>
   <div>
     <AppTitle></AppTitle>
   </div> 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <h1 class="title-card">
+                Hello World!
+            </h1>
+            <div class="text">
+                Questo è il mio jumbotron!
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.container{
+    background-image: url(../assets/MK4251M.jpg);
+    background-size: cover;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+.card{
+    background-color: rgba(39, 32, 32, 0.645);
+    border-radius: 20px;
+    color: white;
+    font-size: 20px;
+}
+
+.title-card{
+    border-top: 1px solid currentColor;
+}
+
+.text{
+    border-bottom: 1px solid currentColor;
+}
+
+.title-card,
+.text{
+    padding: 10px;
+    border-radius: 30px;
+}
+</style>
